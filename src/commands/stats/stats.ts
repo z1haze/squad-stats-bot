13acedde4cedc08ffbe22a46e8a8eb0d7fb5c647
z1haze <stephen.hendricks87@gmail.com>
2023-04-27 12:53:02 -0700

    fix stats page bug caused by players being unranked

--- src/commands/stats/stats.ts
+++ src/commands/stats/stats.ts
@@ -88,16 +88,86 @@ export default {
 
     const player: Player = JSON.parse(targetResult);
 
-    const killsRank = (await redis.zrevrank('leaderboard:kills', player.steamId) as number) + 1;
-    const downsRank = (await redis.zrevrank('leaderboard:downs', player.steamId) as number) + 1;
-    const fallsRank = (await redis.zrevrank('leaderboard:falls', player.steamId) as number) + 1;
-    const deathsRank = (await redis.zrevrank('leaderboard:deaths', player.steamId) as number) + 1;
-    const revivesRank = (await redis.zrevrank('leaderboard:revives', player.steamId) as number) + 1;
-    const tksRank = (await redis.zrevrank('leaderboard:tks', player.steamId) as number) + 1;
-    const overallRank = (await redis.zrevrank('leaderboard:rating', player.steamId) as number) + 1;
-    const kdRank = (await redis.zrevrank('leaderboard:kdr', player.steamId) as number) + 1;
-    const idRank = (await redis.zrevrank('leaderboard:idr', player.steamId) as number) + 1;
-    const matchCountRank = (await redis.zrevrank('leaderboard:matchCount', player.steamId) as number) + 1;
+    const killsRank = (await redis.zrevrank('leaderboard:kills', player.steamId) as number);
+    const downsRank = (await redis.zrevrank('leaderboard:downs', player.steamId) as number);
+    const fallsRank = (await redis.zrevrank('leaderboard:falls', player.steamId) as number);
+    const deathsRank = (await redis.zrevrank('leaderboard:deaths', player.steamId) as number);
+    const revivesRank = (await redis.zrevrank('leaderboard:revives', player.steamId) as number);
+    const tksRank = (await redis.zrevrank('leaderboard:tks', player.steamId) as number);
+    const overallRank = (await redis.zrevrank('leaderboard:rating', player.steamId) as number);
+    const kdRank = (await redis.zrevrank('leaderboard:kdr', player.steamId) as number);
+    const idRank = (await redis.zrevrank('leaderboard:idr', player.steamId) as number);
+    const matchCountRank = (await redis.zrevrank('leaderboard:matchCount', player.steamId) as number);
+
+    let overallFieldValue = `${env.EMOJI_RATING} **Overall Rating**: `;
+    overallFieldValue += `\`${(player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.rating, 0) / player.servers.length).toFixed(0)}\``;
+
+    if (overallRank) {
+      overallFieldValue += ` (${overallRank.toLocaleString()}${nth(overallRank + 1)})`;
+    }
+
+    let matchesFieldValue = `${env.EMOJI_MATCHES} **Games Played**: `;
+    matchesFieldValue += `\`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.matchCount, 0).toLocaleString()}\``;
+
+    if (matchCountRank) {
+      matchesFieldValue += ` (${matchCountRank.toLocaleString()}${nth(matchCountRank + 1)})`;
+    }
+
+    let killsFieldValue = `${env.EMOJI_KILL} **Kills**: `;
+    killsFieldValue += `\`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.kills, 0).toLocaleString()}\``;
+
+    if (killsRank) {
+      killsFieldValue += ` (${killsRank.toLocaleString()}${nth(killsRank + 1)})`;
+    }
+
+    let downsFieldValue = `${env.EMOJI_DOWN} **Downs**: `;
+    downsFieldValue += `\`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.downs, 0).toLocaleString()}\``;
+
+    if (downsRank) {
+      downsFieldValue += ` (${downsRank.toLocaleString()}${nth(downsRank + 1)})`;
+    }
+
+    let kdFieldValue = `${env.EMOJI_KD} **K/D**: `;
+    kdFieldValue += `\`${(player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.kdr, 0) / player.servers.length).toFixed(1)}\``;
+
+    if (kdRank) {
+      kdFieldValue += ` (${kdRank.toLocaleString()}${nth(kdRank + 1)})`;
+    }
+
+    let idFieldValue = `${env.EMOJI_ID} **I/D**: `;
+    idFieldValue += `\`${(player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.idr, 0) / player.servers.length).toFixed(1)}\``;
+
+    if (idRank) {
+      idFieldValue += ` (${idRank.toLocaleString()}${nth(idRank + 1)})`;
+    }
+
+    let revivesFieldValue = `${env.EMOJI_REVIVE} **Revives**: `;
+    revivesFieldValue += `\`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.revives, 0).toLocaleString()}\``;
+
+    if (revivesRank) {
+      revivesFieldValue += ` (${revivesRank.toLocaleString()}${nth(revivesRank + 1)})`;
+    }
+
+    let fallsFieldValue = `${env.EMOJI_FALL} **Falls**: `;
+    fallsFieldValue += `\`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.falls, 0).toLocaleString()}\``;
+
+    if (fallsRank) {
+      fallsFieldValue += ` (${fallsRank.toLocaleString()}${nth(fallsRank + 1)})`;
+    }
+
+    let deathsFieldValue = `${env.EMOJI_DEATH} **Deaths**: `;
+    deathsFieldValue += `\`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.deaths, 0).toLocaleString()}\``;
+
+    if (deathsRank) {
+      deathsFieldValue += ` (${deathsRank.toLocaleString()}${nth(deathsRank + 1)})`;
+    }
+
+    let tksFieldValue = `${env.EMOJI_TK} **Teamkills**: `;
+    tksFieldValue += `\`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.tks, 0).toLocaleString()}\``;
+
+    if (tksRank) {
+      tksFieldValue += ` (${tksRank.toLocaleString()}${nth(tksRank + 1)})`;
+    }
 
     const embed = new EmbedBuilder()
       .setColor('Blurple')
@@ -107,25 +177,25 @@ export default {
       .addFields(
         {
           name: " ",
-          value: `${env.EMOJI_RATING} **Overall Rating**: \`${(player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.rating, 0) / player.servers.length).toFixed(0)}\` (${overallRank.toLocaleString()}${nth(overallRank)})\n${env.EMOJI_MATCHES} **Games Played**: \`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.matchCount, 0).toLocaleString()}\` (${matchCountRank.toLocaleString()}${nth(matchCountRank)})`
+          value: `${overallFieldValue}\n${matchesFieldValue}`
         },
         {
           name: " ",
-          value: `${env.EMOJI_KILL} **Kills**: \`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.kills, 0).toLocaleString()}\`  (${killsRank.toLocaleString()}${nth(killsRank)})\n${env.EMOJI_DOWN} **Downs**: \`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.downs, 0).toLocaleString()}\` (${downsRank.toLocaleString()}${nth(downsRank)})`,
+          value: `${killsFieldValue}\n${downsFieldValue}`,
           inline: true
         },
         {
           name: " ",
-          value: `${env.EMOJI_KD} **K/D**: \`${(player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.kdr, 0) / player.servers.length).toFixed(1)}\` (${kdRank.toLocaleString()}${nth(kdRank)})\n${env.EMOJI_ID} **I/D**: \`${(player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.idr, 0) / player.servers.length).toFixed(1)}\` (${idRank.toLocaleString()}${nth(idRank)})`,
+          value: `${kdFieldValue}\n${idFieldValue}`,
           inline: true
         },
         {
           name: " ",
-          value: `${env.EMOJI_REVIVE} **Revives**: \`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.revives, 0).toLocaleString()}\` (${revivesRank.toLocaleString()}${nth(revivesRank)})`
+          value: `${revivesFieldValue}`
         },
         {
           name: " ",
-          value: `${env.EMOJI_FALL} **Downed**: \`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.falls, 0).toLocaleString()}\` (${fallsRank.toLocaleString()}${nth(fallsRank)})\n${env.EMOJI_DEATH} **Deaths**: \`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.deaths, 0).toLocaleString()}\` (${deathsRank.toLocaleString()}${nth(deathsRank)})\n${env.EMOJI_TK} **Teamkills**: \`${player.servers.reduce((acc: number, curr: PlayerServer) => acc + curr.tks, 0).toLocaleString()}\` (${tksRank.toLocaleString()}${nth(tksRank)})`
+          value: `${fallsFieldValue}\n${deathsFieldValue}\n${tksFieldValue}`
         }
       );
 
